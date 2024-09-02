0bc4577b16d68a846e2667c22216a7a7116c0636
SD-PARK <aa55235490@gmail.com>
2024-09-02 20:38:17 +0900

    SpringBoot Server Settings

---
+++ src/main/java/org/backrow/solt/SoltApplication.java
@@ -0,0 +1,13 @@
+package org.backrow.solt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoltApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/backrow/solt/SoltApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backrow.solt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
