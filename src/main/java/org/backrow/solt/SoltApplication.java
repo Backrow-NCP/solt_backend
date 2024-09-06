7f71a80eb3f2a8d77469e46c13355c5a38ea64f5
SD-PARK <tkdeh129@naver.com>
2024-09-07 00:43:34 +0900

    refact: 로그 제거

--- src/main/java/org/backrow/solt/SoltApplication.java
+++ src/main/java/org/backrow/solt/SoltApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SoltApplication {
     public static void main(String[] args) {
         SpringApplication.run(SoltApplication.class, args);
-
-        System.out.println("Hello World!");
     }
 }
