6a316df108267f7c74cc399f3a0c2fade5c2505e
SD-PARK <aa55235490@gmail.com>
2024-09-06 16:00:03 +0900

    clear test code

--- src/main/java/org/backrow/solt/SoltApplication.java
+++ src/main/java/org/backrow/solt/SoltApplication.java
@@ -1,17 +1,11 @@
 package org.backrow.solt;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Log4j2
 public class SoltApplication {
     public static void main(String[] args) {
-        System.out.println("Hello SD");
         SpringApplication.run(SoltApplication.class, args);
-
-        System.out.println("This is Suhan");
-        log.info("TEST1 @@@@@eateteaetewat@@@@@@======eafefae======@");
     }
 }
