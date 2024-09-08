8d74535bedd2439d33532f348eaee138db9e582e
SD-PARK <tkdeh129@naver.com>
2024-09-08 15:29:53 +0900

    refact: 미사용 메서드 제거

--- src/main/java/org/backrow/solt/dto/UploadResultDTO.java
+++ src/main/java/org/backrow/solt/dto/UploadResultDTO.java
@@ -9,12 +9,4 @@ public class UploadResultDTO {
     private String uuid;
     private String fileName;
     private boolean image;
-
-    public String getLink() {
-        String result = uuid + "_" + fileName;
-
-        if (image) result = "s_" + result;
-
-        return result;
-    }
 }
