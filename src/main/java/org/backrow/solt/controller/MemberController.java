9e0422e6e6b82e28f472dc7d3a960e8c956f7161
SD-PARK <tkdeh129@naver.com>
2024-09-15 12:06:08 +0900

    refact: 썸네일 이미지 업로드 관련 로직 fileService의 로직으로 변경

--- src/main/java/org/backrow/solt/controller/MemberController.java
+++ src/main/java/org/backrow/solt/controller/MemberController.java
@@ -3,16 +3,18 @@ package org.backrow.solt.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.backrow.solt.dto.ImageDTO;
-import org.backrow.solt.dto.ImageUploadDTO;
 import org.backrow.solt.dto.MemberInfoDTO;
 import org.backrow.solt.dto.ModifyDTO;
+import org.backrow.solt.dto.file.UploadResultDTO;
+import org.backrow.solt.service.FileService;
 import org.backrow.solt.service.MemberService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,6 +24,7 @@ import java.util.Map;
 public class MemberController {
 
     private final MemberService memberService;
+    private final FileService fileService;
 
     @Operation(summary = "Member정보 가져오기 GET", description = "GET 회원정보")
     @GetMapping("/info")
@@ -65,10 +68,10 @@ public class MemberController {
 
     @Operation(summary = "회원 프로필 사진 수정", description = "POST 회원 프로필 사진 수정")
     @PutMapping(value ="/modifyImage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Map<String,Boolean>> modifyMemberImage(long memberId,@ModelAttribute ImageUploadDTO imageUploadDTO) {
+    public ResponseEntity<Map<String,Boolean>> modifyMemberImage(long memberId,@RequestPart("image") MultipartFile image) {
         Map<String, Boolean> response = new HashMap<>();
         try {
-            ImageDTO imageDTO = memberService.uploadImage(imageUploadDTO);
+            UploadResultDTO imageDTO = fileService.uploadFile(List.of(image)).get(0);
             memberService.modifyMemberImage(memberId, imageDTO);
             response.put("result", true);
             return ResponseEntity.ok(response);
--- src/main/java/org/backrow/solt/dto/ImageUploadDTO.java
+++
@@ -1,9 +0,0 @@
-package org.backrow.solt.dto;
-
-import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-
-@Data
-public class ImageUploadDTO {
-    private MultipartFile image;
-}
