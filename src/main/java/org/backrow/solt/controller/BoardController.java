f1b5182a8a96bb539acaf66e8e1736e4efe943e1
SD-PARK <tkdeh129@naver.com>
2024-09-15 13:15:24 +0900

    refact: 전역 예외 처리 로직 분리

--- src/main/java/org/backrow/solt/controller/BoardController.java
+++ src/main/java/org/backrow/solt/controller/BoardController.java
@@ -10,7 +10,6 @@ import org.backrow.solt.dto.board.BoardInputDTO;
 import org.backrow.solt.dto.board.BoardViewDTO;
 import org.backrow.solt.service.BoardService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,69 +26,35 @@ public class BoardController {
     @Operation(summary = "게시글 목록 조회", description = "게시판의 모든 게시글 목록을 페이지로 조회합니다.")
     @GetMapping("/list")
     public ResponseEntity<PageResponseDTO<BoardViewDTO>> getBoardList(PageRequestDTO pageRequestDTO) {
-        try {
-            PageResponseDTO<BoardViewDTO> result = boardService.getBoardList(pageRequestDTO);
-            return ResponseEntity.ok(result);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
+        PageResponseDTO<BoardViewDTO> result = boardService.getBoardList(pageRequestDTO);
+        return ResponseEntity.ok(result);
     }
 
     @Operation(summary = "게시글 조회", description = "ID를 통해 특정 게시글을 조회합니다.")
     @GetMapping("/{id}")
     public ResponseEntity<BoardViewDTO> getBoard(@PathVariable Long id) {
-        try {
-            return ResponseEntity.ok(boardService.getBoard(id));
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
+        return ResponseEntity.ok(boardService.getBoard(id));
     }
 
     @Operation(summary = "게시글 작성", description = "새로운 게시글을 작성합니다.")
     @PostMapping
-    public ResponseEntity<Map<String, Long>> saveBoard(
-            @Valid @RequestBody BoardInputDTO boardInputDTO,
-            BindingResult bindingResult
-    ) {
-        if (bindingResult.hasErrors()) {
-            ResponseEntity.badRequest().build();
-        }
-        try {
-            Long id = boardService.saveBoard(boardInputDTO);
-            return ResponseEntity.ok(Map.of("id", id));
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
+    public ResponseEntity<Map<String, Long>> saveBoard(@Valid @RequestBody BoardInputDTO boardInputDTO) {
+        Long id = boardService.saveBoard(boardInputDTO);
+        return ResponseEntity.ok(Map.of("id", id));
     }
 
     @Operation(summary = "게시글 수정", description = "ID를 통해 특정 게시글을 수정합니다.")
     @PutMapping("/{id}")
     public Map<String, Boolean> modifyBoard(
             @PathVariable Long id,
-            @RequestBody BoardInputDTO boardInputDTO,
-            BindingResult bindingResult
+            @RequestBody BoardInputDTO boardInputDTO
     ) {
-        if (bindingResult.hasErrors()) {
-            return Map.of("isSuccess", false);
-        }
-        try {
-            return Map.of("isSuccess", boardService.modifyBoard(id, boardInputDTO));
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return Map.of("isSuccess", false);
-        }
+        return Map.of("isSuccess", boardService.modifyBoard(id, boardInputDTO));
     }
 
     @Operation(summary = "게시글 삭제", description = "ID를 통해 특정 게시글을 삭제합니다.")
     @DeleteMapping("/{id}")
     public Map<String, Boolean> deleteBoard(@PathVariable Long id) {
-        try {
-            return Map.of("isSuccess", boardService.deleteBoard(id));
-        } catch (Exception e) {
-            return Map.of("isSuccess", false);
-        }
+        return Map.of("isSuccess", boardService.deleteBoard(id));
     }
 }
