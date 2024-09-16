5bf574ba991c87ab4f7d4f06275a8079852f0f2a
SD-PARK <tkdeh129@naver.com>
2024-09-16 18:11:33 +0900

    feat: 게시글 응답 시 게시자, 이미지, 좋아요 개수 함께 반환

--- src/main/java/org/backrow/solt/repository/search/BoardSearchImpl.java
+++ src/main/java/org/backrow/solt/repository/search/BoardSearchImpl.java
@@ -1,21 +1,24 @@
 package org.backrow.solt.repository.search;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.QBean;
+import com.querydsl.core.types.QList;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
-import org.backrow.solt.domain.Board;
-import org.backrow.solt.domain.QBoard;
-import org.backrow.solt.domain.QLikeLog;
-import org.backrow.solt.domain.QMember;
+import org.backrow.solt.domain.*;
+import org.backrow.solt.dto.board.BoardImageDTO;
 import org.backrow.solt.dto.board.BoardViewDTO;
+import org.backrow.solt.dto.member.MemberInfoDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardSearch {
     public BoardSearchImpl() {
@@ -29,8 +32,8 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
         QMember member = QMember.member;
 
         JPQLQuery<Board> boardQuery = from(board)
-                .leftJoin(likeLog).on(likeLog.board.eq(board))
                 .leftJoin(member).on(board.member.eq(member))
+                .leftJoin(likeLog).on(likeLog.board.eq(board))
                 .groupBy(board);
 
         if (types != null) {
@@ -51,27 +54,33 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
             }
         }
 
-        JPQLQuery<BoardViewDTO> boardViewQuery = boardQuery.select(createBoardViewDTOProjection(board, likeLog));
+        this.getQuerydsl().applyPagination(pageable, boardQuery);
+        List<Board> boardEntities = boardQuery.fetch();
+        List<BoardViewDTO> boardViewDTOS = boardEntities.stream()
+                .map(boardEntity -> createBoardViewDTO(boardEntity, false))
+                .collect(Collectors.toList());
+        long listCount = boardViewDTOS.size();
 
-        this.getQuerydsl().applyPagination(pageable, boardViewQuery);
-        List<BoardViewDTO> list = boardViewQuery.fetch();
-        long listCount = list.size();
-
-        return new PageImpl<>(list, pageable, listCount);
+        return new PageImpl<>(boardViewDTOS, pageable, listCount);
     }
 
     @Override
     public BoardViewDTO searchBoardView(Long boardId) {
         QBoard board = QBoard.board;
         QLikeLog likeLog = QLikeLog.likeLog;
+        QMember member = QMember.member;
+        QBoardImage boardImage = QBoardImage.boardImage;
 
-        JPQLQuery<BoardViewDTO> boardViewQuery = from(board)
+        JPQLQuery<Board> boardQuery = from(board)
+                .leftJoin(member).on(board.member.eq(member))
+                .leftJoin(boardImage).on(boardImage.board.eq(board))
                 .leftJoin(likeLog).on(likeLog.board.eq(board))
                 .where(board.boardId.eq(boardId))
-                .groupBy(board)
-                .select(createBoardViewDTOProjection(board, likeLog));
+                .groupBy(board);
 
-        return boardViewQuery.fetchOne();
+        Board boardEntity = boardQuery.fetchOne();
+
+        return createBoardViewDTO(boardEntity, true);
     }
 
     private BooleanExpression titleContain(String keyword) {
@@ -86,13 +95,35 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
         return keyword != null ? QBoard.board.member.name.containsIgnoreCase(keyword) : null;
     }
 
-    private ConstructorExpression<BoardViewDTO> createBoardViewDTOProjection(QBoard board, QLikeLog likeLog) {
-        return Projections.constructor(BoardViewDTO.class,
-                board.boardId,
-                board.title,
-                board.content,
-                likeLog.count().intValue().coalesce(0).as("likeCount"),
-                board.regDate,
-                board.modDate);
+    private BoardViewDTO createBoardViewDTO(Board board, boolean includeImage) {
+        BoardViewDTO.BoardViewDTOBuilder builder = BoardViewDTO.builder();
+
+        MemberInfoDTO memberInfoDTO = MemberInfoDTO.builder()
+                .memberId(board.getMember().getMemberId())
+                .name(board.getMember().getName())
+                .build();
+
+        if (includeImage) {
+            List<BoardImageDTO> boardImageDTOS = board.getBoardImages().stream()
+                    .map(boardImageEntity -> BoardImageDTO.builder()
+                            .uuid(boardImageEntity.getUuid())
+                            .fileName(boardImageEntity.getFileName())
+                            .ord(boardImageEntity.getOrd())
+                            .build())
+                    .collect(Collectors.toList());
+            builder.images(boardImageDTOS);
+        }
+
+        int likeCount = board.getLikeLog() != null ? board.getLikeLog().size() : 0;
+
+        return builder
+                .boardId(board.getBoardId())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .member(memberInfoDTO)
+                .likeCount(likeCount)
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .build();
     }
 }
