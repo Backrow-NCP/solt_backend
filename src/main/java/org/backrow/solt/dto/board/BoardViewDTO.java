35eaf2414ef63038ab2e21359429d751a3b9676f
SD-PARK <tkdeh129@naver.com>
2024-09-16 18:11:33 +0900

    feat: 게시글 응답 시 게시자, 이미지, 좋아요 개수 함께 반환

--- src/main/java/org/backrow/solt/dto/board/BoardViewDTO.java
+++ src/main/java/org/backrow/solt/dto/board/BoardViewDTO.java
@@ -1,20 +1,25 @@
 package org.backrow.solt.dto.board;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.backrow.solt.dto.member.MemberInfoDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class BoardViewDTO {
         private Long boardId;
         private String title;
         private String content;
-//        private UserDTO user;
 //        private PlanDTO plan;
+        private MemberInfoDTO member;
+        private List<BoardImageDTO> images;
         private Integer likeCount;
         private LocalDateTime regDate;
         private LocalDateTime modDate;
