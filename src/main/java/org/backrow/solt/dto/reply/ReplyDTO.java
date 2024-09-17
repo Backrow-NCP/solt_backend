ee019d6d1917286302c981f8bcc443950671addb
SD-PARK <tkdeh129@naver.com>
2024-09-17 15:56:01 +0900

    refact: 댓글 작성, 수정 전용 DTO 추가

--- src/main/java/org/backrow/solt/dto/reply/ReplyDTO.java
+++ src/main/java/org/backrow/solt/dto/reply/ReplyDTO.java
@@ -11,18 +11,10 @@ import java.time.LocalDateTime;
 @Data
 public class ReplyDTO {
     private Long replyId;
-
-    @NotEmpty(message = "본문은 반드시 존재해야 합니다.")
-    @Size(min = 1, max = 500, message = "본문은 1~500자 이내의 문자열이어야 합니다.")
     private String content;
-
-    @NotNull(message = "게시글 ID는 반드시 존재해야 합니다.")
     private Long boardId;
-
     private MemberInfoDTO member;
-
     private Long parentReplyId;
-
     private LocalDateTime regDate;
     private LocalDateTime modDate;
 }
