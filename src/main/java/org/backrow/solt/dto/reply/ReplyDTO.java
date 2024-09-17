a5c1bbf394910a20ba24e2fc0d63a498128db32a
SD-PARK <tkdeh129@naver.com>
2024-09-17 16:13:28 +0900

    style: 사용되지 않는 import 문 제거

--- src/main/java/org/backrow/solt/dto/reply/ReplyDTO.java
+++ src/main/java/org/backrow/solt/dto/reply/ReplyDTO.java
@@ -2,10 +2,6 @@ package org.backrow.solt.dto.reply;
 
 import lombok.Data;
 import org.backrow.solt.dto.member.MemberInfoDTO;
-
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
