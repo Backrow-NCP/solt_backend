4a2f8fceec485029943c44bc038adc7a4ca7d219
NoskeLim <NoskeLim>
2024-09-20 17:37:58 +0900

    Feat: Repository에 JPA 연결

--- src/main/java/org/backrow/solt/repository/PlaceRepository.java
+++ src/main/java/org/backrow/solt/repository/PlaceRepository.java
@@ -1,9 +1,9 @@
 package org.backrow.solt.repository;
 
-import org.backrow.solt.dto.PlanDTO;
+import org.backrow.solt.dto.PlaceDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanRepository extends JpaRepository<PlanDTO, Integer> {
+public interface PlaceRepository extends JpaRepository<PlaceDTO, Integer> {
 }
