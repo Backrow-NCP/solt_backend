4802e85701579bec9fd3ab7cd91fb4b2978dfb0c
NoskeLim <NoskeLim>
2024-09-23 21:14:44 +0900

    feat: JpaRepository의 Long -> Integer 변경
    - 판단기준이 int 타입의 PlanId이어서 변경

--- src/main/java/org/backrow/solt/repository/PlanRepository.java
+++ src/main/java/org/backrow/solt/repository/PlanRepository.java
@@ -3,5 +3,5 @@ package org.backrow.solt.repository;
 import org.backrow.solt.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlanRepository extends JpaRepository<Plan, Long> {
+public interface PlanRepository extends JpaRepository<Plan, Integer> {
 }
