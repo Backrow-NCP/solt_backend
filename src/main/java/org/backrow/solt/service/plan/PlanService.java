b586c7b90d44b78906a28f9e804e45981f3b005d
NoskeLim <NoskeLim>
2024-09-23 21:11:22 +0900

    feat: PlanService에 기능 추가

--- src/main/java/org/backrow/solt/service/plan/PlanService.java
+++ src/main/java/org/backrow/solt/service/plan/PlanService.java
@@ -5,10 +5,12 @@ import org.backrow.solt.dto.page.PageRequestDTO;
 import org.backrow.solt.dto.page.PageResponseDTO;
 
 public interface PlanService {
+
     PlanDTO getPlan(int planId);
     PageResponseDTO<PlanDTO> getPlanList(PageRequestDTO pageRequestDTO);
     boolean deletePlan(int planId);
-    boolean modifyPlan(PlanDTO planDTO);
+    boolean modifyPlan(int planId, PlanDTO planDTO);
     long savePlan(PlanDTO planDTO);
     PlanDTO aiRecommend(PlanDTO planDTO);
+
 }
