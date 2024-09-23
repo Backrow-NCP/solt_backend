421f13c5b46c5df02466fef0cb929f01858025a4
NoskeLim <NoskeLim>
2024-09-23 21:37:38 +0900

    Feat: PageRequestDTO 수정

--- src/main/java/org/backrow/solt/dto/page/PageRequestDTO.java
+++ src/main/java/org/backrow/solt/dto/page/PageRequestDTO.java
@@ -1,26 +1,35 @@
 package org.backrow.solt.dto.page;
 
-import lombok.Data;
+import lombok.*;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageRequestDTO {
-    private int page;
-    private int size;
-    private Sort.Direction direction;
-    private String sortBy;
 
-    public PageRequestDTO() {
-        this.page = 1; // 기본값 1페이지
-        this.size = 10; // 기본값 한 페이지당 10개 항목
-        this.direction = Sort.Direction.DESC; // 기본 정렬은 내림차순
-        this.sortBy = "id"; // 기본 정렬 기준은 id
+    @Builder.Default
+    private int page = 1;
+
+    @Builder.Default
+    private int size = 10;
+
+    private String type;
+
+    private String keyword;
+
+    public String[] getTypes(){
+        if(type == null || type.isEmpty())
+            return null;
+        return type.split("");
     }
 
-    // 현재 페이지 요청에 맞는 Pageable 객체를 반환하는 메서드
-    public org.springframework.data.domain.Pageable getPageable() {
-        return org.springframework.data.domain.PageRequest.of(
-                page - 1, size, Sort.by(direction, sortBy));
+    public Pageable getPageable(String ...props){
+        return PageRequest.of(this.page - 1, this.size, Sort.by(props).descending());
     }
 }
 
