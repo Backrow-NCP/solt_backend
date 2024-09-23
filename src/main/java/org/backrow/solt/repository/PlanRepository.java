b93a08e9bdc51834607c03c9a3ed5d9fcac823ab
NoskeLim <NoskeLim>
2024-09-23 19:13:38 +0900

    Feat: PlanRepository에 JPA 주입

--- src/main/java/org/backrow/solt/repository/PlanRepository.java
+++ src/main/java/org/backrow/solt/repository/PlanRepository.java
@@ -1,9 +1,9 @@
 package org.backrow.solt.repository;
 
-import org.backrow.solt.dto.PlanDTO;
+import org.backrow.solt.model.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanRepository extends JpaRepository<PlanDTO, Integer> {
+public interface PlanRepository extends JpaRepository<Plan, Long> {
 }
