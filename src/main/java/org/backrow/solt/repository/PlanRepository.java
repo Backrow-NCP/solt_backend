27349a5df45ad2612ea9a7651ec8830213ec8fac
NoskeLim <NoskeLim>
2024-09-23 21:13:33 +0900

    feat: JpaRepository의 Integer -> Long 변환

--- src/main/java/org/backrow/solt/repository/PlanRepository.java
+++ src/main/java/org/backrow/solt/repository/PlanRepository.java
@@ -1,9 +1,7 @@
 package org.backrow.solt.repository;
 
-import org.backrow.solt.model.Plan;
+import org.backrow.solt.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 }
