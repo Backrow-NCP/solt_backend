249a414b246268441399b6cb55be06a1be575a55
NoskeLim <NoskeLim>
2024-09-24 21:36:19 +0900

    Feat: AI Plan 관련 클래스 수정

--- src/main/java/org/backrow/solt/service/ai/AIPlanMaker.java
+++ src/main/java/org/backrow/solt/service/ai/AIPlanMaker.java
@@ -1,11 +1,11 @@
 package org.backrow.solt.service.ai;
 
 import org.backrow.solt.dto.ClovaDTO;
-import org.backrow.solt.dto.PlanDTO;
+import org.backrow.solt.dto.plan.PlanDTO;
 
 import java.util.List;
 
 public interface AIPlanMaker {
-    PlanDTO aiRecommend();
+    PlanDTO aiRecommend(PlanDTO planDTO);
     List<ClovaDTO> runFetching();
 }
--- src/main/java/org/backrow/solt/service/ai/AIPlanMakerImpl.java
+++ src/main/java/org/backrow/solt/service/ai/AIPlanMakerImpl.java
@@ -1,23 +1,35 @@
 package org.backrow.solt.service.ai;
 
 import org.backrow.solt.dto.ClovaDTO;
-import org.backrow.solt.dto.PlanDTO;
+import org.backrow.solt.dto.plan.PlanDTO;
+import org.backrow.solt.fetcher.FetchManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class AIPlanMakerImpl implements AIPlanMaker {
 
+    private final FetchManager fetchManager;
+
+    @Autowired
+    public AIPlanMakerImpl(FetchManager fetchManager) {
+        this.fetchManager = fetchManager;
+    }
+
     @Override
-    public PlanDTO aiRecommend() {
+    public PlanDTO aiRecommend(PlanDTO planDTO) {
         // AI 추천 알고리즘 로직
         return null;
     }
 
     @Override
     public List<ClovaDTO> runFetching() {
-        // 외부 API와 통신하는 로직
-        return null;
+        fetchManager.runFetching();
+        // ClovaDTO 객체를 가져오는 로직 
+        return new ArrayList<ClovaDTO>();
     }
 }
 
