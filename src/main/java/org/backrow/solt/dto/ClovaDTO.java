b1f53e00589695ea42c3200e683ccbba1b6cc57b
NoskeLim <NoskeLim>
2024-09-26 10:48:05 +0900

    Remove: 작성이 잘못된 클래스들 삭제
    - ClovaDTO.java
    - PlaceRepository.java
    - ThemeDTO.java

--- src/main/java/org/backrow/solt/dto/ClovaDTO.java
+++
@@ -1,7 +0,0 @@
-package org.backrow.solt.dto;
-
-import lombok.Data;
-
-@Data
-public class ClovaDTO {
-}
--- src/main/java/org/backrow/solt/repository/PlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.backrow.solt.repository;
-
-import org.backrow.solt.dto.PlaceDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlaceRepository extends JpaRepository<PlaceDTO, Integer> {
-}
