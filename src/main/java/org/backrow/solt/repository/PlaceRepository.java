83854d618b35ddace194879b0232a1bd21103721
NoskeLim <NoskeLim>
2024-09-26 11:02:26 +0900

    Feat: Repository 클래스 추가
    - PlaceRepository
    - RouteRepository

---
+++ src/main/java/org/backrow/solt/repository/PlaceRepository.java
@@ -0,0 +1,9 @@
+package org.backrow.solt.repository;
+
+import org.backrow.solt.domain.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaceRepository extends JpaRepository<Route, Long> {
+}
