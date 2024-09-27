8a9d17d71d92b7e0bde42404f9e61d489d6c3cba
NoskeLim <NoskeLim>
2024-09-27 11:00:26 +0900

    Feat: Plan Repository 클래스 수정
    - @Repository 어노테이션 삽입

--- src/main/java/org/backrow/solt/repository/PlanRepository.java
+++ src/main/java/org/backrow/solt/repository/PlanRepository.java
@@ -2,6 +2,8 @@ package org.backrow.solt.repository;
 
 import org.backrow.solt.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlanRepository extends JpaRepository<Plan, Integer> {
 }
