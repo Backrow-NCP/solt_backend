6d000cc913ec69de3933f8fa7dba56fce2686623
SD-PARK <aa55235490@gmail.com>
2024-10-07 09:28:30 +0900

    feat: Swagger 내 인증 설정 추가

--- src/main/java/org/backrow/solt/config/SwaggerConfig.java
+++ src/main/java/org/backrow/solt/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package org.backrow.solt.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +20,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("SOLT API 문서")
                         .version("0.0.1")
-                        .description("SOLT 시스템에 대한 API 문서입니다."));
+                        .description("SOLT 시스템에 대한 API 문서입니다."))
+                .addSecurityItem(securityRequirement)
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme));
     }
 }
