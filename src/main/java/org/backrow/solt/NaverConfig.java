8b619d1766f2210a386f3ef2fc0abca4a32c8c68
jisu.park <jisu.park9191@gmail.com>
2024-10-15 16:40:29 +0900

    Fix: redis의 host를 container이름으로 변경

--- src/main/java/org/backrow/solt/NaverConfig.java
+++ src/main/java/org/backrow/solt/NaverConfig.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.PropertySource;
 @Setter
 @ToString
 public class NaverConfig {
-  // ncp.* 이름으로 된 프로퍼티 값을 받을 필드를 선언한다.
   private String endPoint; // ncp.endPoint 프로퍼티를 받는 필드
   private String regionName; // ncp.regionName 프로퍼티를 받는 필드
   private String accessKey; // ncp.accessKey 프로퍼티를 받는 필드
