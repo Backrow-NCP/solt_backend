8a409ce37b179bee461a3d8fd3e483261db6add9
jisu.park <jisu.park9191@gmail.com>
2024-10-15 14:47:21 +0900

    Fix: EmailController에 로그추가

--- src/main/java/org/backrow/solt/controller/MailController.java
+++ src/main/java/org/backrow/solt/controller/MailController.java
@@ -1,5 +1,7 @@
 package org.backrow.solt.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.backrow.solt.service.MailCheckService;
@@ -11,6 +13,7 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
+@Tag(name = "이메일인증 API", description = "이메일 인증번호 보내기, 인증번호 확인 ")
 @RestController
 @RequestMapping("/email")
 @RequiredArgsConstructor
@@ -19,8 +22,10 @@ public class MailController {
 
     private final MailCheckService mailCheckService;
 
+    @Operation(summary="mail인증번호 보내기", description ="POST 요청으로 메일 인증번호 보내기")
     @PostMapping("/verification-requests")
-    public  ResponseEntity<Map<String, Boolean>> sendMessage(@RequestParam("email") @Valid String email) {
+    public  ResponseEntity<Map<String, Boolean>> sendMessage(@RequestParam("email") String email) {
+        log.info("Send verification number : " + email);
         Map<String, Boolean> response = new HashMap<>();
         try {
             mailCheckService.sendCodeToEmail(email);
@@ -32,8 +37,9 @@ public class MailController {
         }
     }
 
+    @Operation(summary="인증번호로 인증요청", description ="GET으로 인증요청")
     @GetMapping("/verifications")
-    public ResponseEntity<Map<String, Boolean>> verificationEmail(@RequestParam("email") @Valid String email,
+    public ResponseEntity<Map<String, Boolean>> verificationEmail(@RequestParam("email") String email,
                                                                   @RequestParam("code") String authCode) {
         Boolean response = mailCheckService.verifiedCode(email, authCode);
         Map<String, Boolean> map = new HashMap<>();
