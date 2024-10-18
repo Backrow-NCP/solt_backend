f96ce041630b3d384793d41d6e7dbf23e20664e2
SD-PARK <aa55235490@gmail.com>
2024-10-18 09:10:33 +0900

    rename: 패키지 구조 개선

--- src/main/java/org/backrow/solt/config/mail/EmailConfig.java
+++ src/main/java/org/backrow/solt/config/mail/EmailConfig.java
@@ -1,4 +1,4 @@
-package org.backrow.solt.config;
+package org.backrow.solt.config.mail;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- src/main/java/org/backrow/solt/config/web/CustomServletConfig.java
+++ src/main/java/org/backrow/solt/config/web/CustomServletConfig.java
@@ -1,4 +1,4 @@
-package org.backrow.solt.config;
+package org.backrow.solt.config.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
