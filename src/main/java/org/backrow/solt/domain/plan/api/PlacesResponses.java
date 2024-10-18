247a5456ed8a52267266db0882407d2f92ab1c08
NoskeLim <NoskeLim>
2024-10-18 10:40:31 +0900

    Feat: PlacesResponses 필드 추가

--- src/main/java/org/backrow/solt/domain/plan/api/PlacesResponses.java
+++ src/main/java/org/backrow/solt/domain/plan/api/PlacesResponses.java
@@ -11,14 +11,13 @@ import java.time.LocalDateTime;
 @ToString
 public class PlacesResponses {
 
-    private Long placeId;           // 장소 ID
-    private String placeName;       // 장소 이름
-    private String addr;            // 주소
-    private Integer price;          // 가격 (있다면)
-    private LocalDateTime startTime; // 시작 시간
-    private LocalDateTime endTime;   // 종료 시간
-    private String description;     // 장소 설명
-    private Boolean checker;        // 수정 가능 여부
+    private Long placeId;
+    private String placeName;
+    private String addr;
+    private Integer price;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String description;
+    private Boolean checker;
 
-    // 필요에 따라 추가적인 메서드도 여기에 작성할 수 있음.
 }
