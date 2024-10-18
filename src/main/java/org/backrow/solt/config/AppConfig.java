95dec5e5064a29ee0e41a90349f56d423e296b6c
NoskeLim <NoskeLim>
2024-10-18 21:28:47 +0900

    Feat: @PropertySource 추가

--- src/main/java/org/backrow/solt/config/AppConfig.java
+++ src/main/java/org/backrow/solt/config/AppConfig.java
@@ -11,9 +11,11 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@PropertySource("classpath:.env")
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
