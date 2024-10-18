e82073719373d04e7deb6ebb9fb6682193a8dc6e
NoskeLim <NoskeLim>
2024-10-18 21:29:03 +0900

    Feat: dontenv 추가

--- src/main/java/org/backrow/solt/SoltApplication.java
+++ src/main/java/org/backrow/solt/SoltApplication.java
@@ -1,5 +1,6 @@
 package org.backrow.solt;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -9,6 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class SoltApplication {
+
+    Dotenv dotenv = Dotenv.load();
+
+    // 환경 변수를 가져와서 사용할 수 있습니다.
+    String googleMapsApiKey = dotenv.get("GOOGLE_MAPS_API_KEY");
+    String clovaApiKey = dotenv.get("CLOVA_API_KEY");
+
     public static void main(String[] args) {
         SpringApplication.run(SoltApplication.class, args);
     }
