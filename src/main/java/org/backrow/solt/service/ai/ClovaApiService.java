438c2911015d9a11704bb4f4a4dfeb65091910d9
NoskeLim <NoskeLim>
2024-10-18 17:02:54 +0900

    Feat: body의 content를 가져오는 로직 변경

--- src/main/java/org/backrow/solt/service/ai/ClovaApiService.java
+++ src/main/java/org/backrow/solt/service/ai/ClovaApiService.java
@@ -42,6 +42,7 @@ public class ClovaApiService {
      */
 
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     // 생성자 주입 방식 사용
     public ClovaApiService(RestTemplate restTemplate) {
@@ -50,25 +51,24 @@ public class ClovaApiService {
 
     // Clova API 요청을 보내는 메소드
     public List<PlacesResponses> callClovaApi(PlanInputDTO planInputDTO) {
-
         // 필요한 PlanInputDTO 필드 값 추출
-        String location = planInputDTO.getLocation(); // 지역
-        String startDate = planInputDTO.getStartDate().toString(); // 시작 날짜
-        String endDate = planInputDTO.getEndDate().toString(); // 종료 날짜
+        String location = planInputDTO.getLocation();
+        String startDate = planInputDTO.getStartDate().toString();
+        String endDate = planInputDTO.getEndDate().toString();
 
         // 테마 ID를 ThemeDTO로 변환
         Set<ThemeDTO> themeSet = planInputDTO.getThemes().stream()
                 .map(themeId -> ThemeDTO.builder()
-                        .themeId(themeId)  // Long 값을 ThemeDTO로 변환
+                        .themeId(themeId)
                         .build())
                 .collect(Collectors.toSet());
 
-        Set<PlaceDTO> places = planInputDTO.getPlaces(); // 꼭 가야하는 장소
+        Set<PlaceDTO> places = planInputDTO.getPlaces();
 
         // 테마와 장소를 문자열로 변환
         String themeList = themeSet.stream()
-                .map(ThemeDTO::getThemeId) // 여기서 ID 사용, 나중에 이름 추가 필요
-                .map(String::valueOf) // Long -> String 변환
+                .map(ThemeDTO::getThemeId)
+                .map(String::valueOf)
                 .collect(Collectors.joining(", "));
 
         String placeList = places.stream()
@@ -80,44 +80,10 @@ public class ClovaApiService {
                 location, startDate, endDate, themeList, placeList);
 
         // Request Body
-        String requestBody =
-                "{\n" +
-                        "  \"messages\": [\n" +
-                        "    {\n" +
-                        "      \"role\": \"system\",\n" +
-                        "      \"content\": \"- 지역, 기간, 키워드를 입력하면 여행 일정을 json으로 생성합니다.\\n" +
-                        "      - 하루에 최소 3 장소 이상이 포함되어 있어야합니다.\\n" +
-                        "      - 꼭 가야하는 장소는 포함될 수도 있습니다.\\n" +
-                        "      - 숙소는 반드시 일정에 포함하여야 하고 각 일자별의 마지막에 반드시 포함되어야한다.\\n" +
-                        "      - 마지막 일정에는 근처 공항이 반드시 포함되어 있어야합니다.\\n\\n" +
-                        "      - json의 형식은 다음과 같습니다\\n" +
-                        "      {\\n  \\\"places\\\": [\\r\\n    {\\r\\n      \\\"placeId\\\": 0,\\r\\n      \\\"placeName\\\": \\\"string\\\",\\r\\n      \\\"addr\\\": \\\"string\\\",\\r\\n      \\\"price\\\": 0,\\r\\n      \\\"startTime\\\": \\\"2024-09-15T13:20:00\\\",\\r\\n      \\\"endTime\\\": \\\"2024-09-15T13:20:00\\\",\\r\\n      \\\"description\\\": \\\"string\\\",\\r\\n      \\\"checker\\\": true\\r\\n    }\\r\\n  ],\\r\\n  \\\"themes\\\": [\\r\\n    0\\r\\n  ],\\r\\n  \\\"location\\\": \\\"string\\\",\\r\\n  \\\"startDate\\\": \\\"2024-10-15\\\",\\r\\n  \\\"endDate\\\": \\\"2024-10-15\\\"\\r\\n}\\n\\n" +
-                        "      - places의 date는 날짜 순으로 배열되어야합니다.\\n" +
-                        "      - routes의 startPlaceId와 endPlaceId는 같은 날짜에 있는 장소여야합니다.\\n" +
-                        "      - description은 20자 이내로 제공되어야 합니다.\\n" +
-                        "      - 모든 Id는 순차적으로 0부터 증가해야합니다.\"\n" +
-                        "    },\n" +
-                        "    {\n" +
-                        "      \"role\": \"user\",\n" +
-                        "      \"content\": \"" + " [서울], [2024-10-01],[2024-10-03], [여유롭게 즐겨요, 맛집에 관심있어요, 가성비가 중요해요] \\n [꼭 가야하는 장소] - [세종문화회관], [서울월드컵경기장], [반포한강공원] " + "\"\n" +
-                        "    }\n" +
-                        "  ],\n" +
-                        "  \"topP\": 0.8,\n" +
-                        "  \"topK\": 0,\n" +
-                        "  \"maxTokens\": 3523,\n" +
-                        "  \"temperature\": 0.7,\n" +
-                        "  \"repeatPenalty\": 3.5,\n" +
-                        "  \"stopBefore\": [],\n" +
-                        "  \"includeAiFilters\": true,\n" +
-                        "  \"seed\": 2048\n" +
-                        "}";
+        String requestBody = createRequestBody(userContent);
 
         // Headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-NCP-CLOVASTUDIO-API-KEY", "NTA0MjU2MWZlZTcxNDJiYwBYJNO67mQdsAlDTunBopY34AAw2fGnua3t3a4kFgPm");
-        headers.set("X-NCP-APIGW-API-KEY", "j96OgrLz3aBC78T9HgjgsQvcg5EhYxgWOT0Ot4RK");
-        headers.set("X-NCP-CLOVASTUDIO-REQUEST-ID", "5589e9f2-ef1a-42b8-8b2b-39c6968e83ea");
-        headers.set("Content-Type", "application/json");
+        HttpHeaders headers = createHeaders();
 
         // HttpEntity에 requestBody와 headers 포함
         HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
@@ -131,9 +97,7 @@ public class ClovaApiService {
             );
 
             log.info("Response from Clova API: {}", response.getBody());
-
-            // 응답을 PlaceResponse로 매핑하여 반환
-            return (List<PlacesResponses>) convertToClovaApiResponse(response.getBody());
+            return parseClovaApiResponse(response.getBody());
 
         } catch (HttpClientErrorException e) {
             log.error("Error during Clova API call: {}", e.getMessage());
@@ -144,40 +108,68 @@ public class ClovaApiService {
         }
     }
 
+    private String createRequestBody(String userContent) {
+        return "{\n" +
+                "  \"messages\": [\n" +
+                "    {\n" +
+                "      \"role\": \"system\",\n" +
+                "      \"content\": \"- 지역, 기간, 키워드를 입력하면 여행 일정을 json으로 생성합니다.\\n" +
+                "      - 하루에 최소 3 장소 이상이 포함되어 있어야합니다.\\n" +
+                "      - 꼭 가야하는 장소는 포함될 수도 있습니다.\\n" +
+                "      - 숙소는 반드시 일정에 포함하여야 하고 각 일자별의 마지막에 반드시 포함되어야한다.\\n" +
+                "      - 마지막 일정에는 근처 공항이 반드시 포함되어 있어야합니다.\\n\\n" +
+                "      - json의 형식은 다음과 같습니다\\n" +
+                "      {\\n  \\\"places\\\": [\\r\\n    {\\r\\n      \\\"placeId\\\": 0,\\r\\n      \\\"placeName\\\": \\\"string\\\",\\r\\n      \\\"addr\\\": \\\"string\\\",\\r\\n      \\\"price\\\": 0,\\r\\n      \\\"startTime\\\": \\\"2024-09-15T13:20:00\\\",\\r\\n      \\\"endTime\\\": \\\"2024-09-15T13:20:00\\\",\\r\\n      \\\"description\\\": \\\"string\\\",\\r\\n      \\\"checker\\\": true\\r\\n    }\\r\\n  ],\\r\\n  \\\"themes\\\": [\\r\\n    0\\r\\n  ],\\r\\n  \\\"location\\\": \\\"string\\\",\\r\\n  \\\"startDate\\\": \\\"2024-10-15\\\",\\r\\n  \\\"endDate\\\": \\\"2024-10-15\\\"\\r\\n}\\n\\n" +
+                "      - places의 date는 날짜 순으로 배열되어야합니다.\\n" +
+                "      - routes의 startPlaceId와 endPlaceId는 같은 날짜에 있는 장소여야합니다.\\n" +
+                "      - description은 20자 이내로 제공되어야 합니다.\\n" +
+                "      - 모든 Id는 순차적으로 0부터 증가해야합니다.\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"role\": \"user\",\n" +
+                "      \"content\": \"" + "[서울], [2024-10-01],[2024-10-03], [여유롭게 즐겨요, 맛집에 관심있어요, 가성비가 중요해요] \\n [꼭 가야하는 장소] - [세종문화회관], [서울월드컵경기장], [반포한강공원] " + "\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"topP\": 0.8,\n" +
+                "  \"topK\": 0,\n" +
+                "  \"maxTokens\": 3523,\n" +
+                "  \"temperature\": 0.7,\n" +
+                "  \"repeatPenalty\": 3.5,\n" +
+                "  \"stopBefore\": [],\n" +
+                "  \"includeAiFilters\": true,\n" +
+                "  \"seed\": 2048\n" +
+                "}";
+    }
+
+    private HttpHeaders createHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-NCP-CLOVASTUDIO-API-KEY", "NTA0MjU2MWZlZTcxNDJiYwBYJNO67mQdsAlDTunBopY34AAw2fGnua3t3a4kFgPm");
+        headers.set("X-NCP-APIGW-API-KEY", "j96OgrLz3aBC78T9HgjgsQvcg5EhYxgWOT0Ot4RK");
+        headers.set("X-NCP-CLOVASTUDIO-REQUEST-ID", "5589e9f2-ef1a-42b8-8b2b-39c6968e83ea");
+        headers.set("Content-Type", "application/json");
+        return headers;
+    }
+
     // 응답 문자열을 ClovaApiResponse 객체로 변환하는 메소드
-    public static ClovaApiResponse convertToClovaApiResponse(String responseBody) {
-        ObjectMapper objectMapper = new ObjectMapper();
-
+    private List<PlacesResponses> parseClovaApiResponse(String responseBody) {
         try {
-            // JSON 문자열을 ClovaApiResponse로 변환
             ClovaApiResponse clovaApiResponse = objectMapper.readValue(responseBody, ClovaApiResponse.class);
-
-            // content에서 places 추출
             String content = clovaApiResponse.getResult().getMessage().getContent();
-            List<PlacesResponses> placeResponses = parseContent(content);
-
-            // 추출한 places 리스트를 ClovaApiResponse에 설정
-            clovaApiResponse.setPlaces(placeResponses);
-
-            return clovaApiResponse;
-
+            return parsePlaces(content);
         } catch (Exception e) {
             log.error("Failed to parse response body: {}", e.getMessage());
             throw new RuntimeException("Failed to parse ClovaApiResponse", e);
         }
     }
 
-    // 응답 문자열을 파싱하여 PlaceResponse 리스트로 변환하는 메소드
-    private static List<PlacesResponses> parseContent(String content) {
-        ObjectMapper objectMapper = new ObjectMapper();
+    // content에서 places를 추출하는 메소드
+    private List<PlacesResponses> parsePlaces(String content) {
         List<PlacesResponses> placeResponses = new ArrayList<>();
 
         try {
-            // JSON 문자열을 JsonNode로 변환
             JsonNode rootNode = objectMapper.readTree(content);
-
-            // places 배열 파싱
             JsonNode placesNode = rootNode.path("places");
+
             for (JsonNode placeNode : placesNode) {
                 PlacesResponses placeResponse = new PlacesResponses();
                 placeResponse.setPlaceId(placeNode.path("placeId").asLong());
@@ -191,9 +183,9 @@ public class ClovaApiService {
 
                 placeResponses.add(placeResponse);
             }
+
         } catch (Exception e) {
-            log.error("Failed to parse content: {}", e.getMessage());
-            throw new RuntimeException("Failed to parse PlaceResponses", e);
+            log.error("Error parsing places: {}", e.getMessage());
         }
 
         return placeResponses;
