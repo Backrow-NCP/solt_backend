ce043dabb1f0cc326bea999ecfabe3b61755c46d
NoskeLim <NoskeLim>
2024-10-18 15:13:31 +0900

    Feat: Clova API 응답에 맞춘 DTO 클래스 Type에 맞게 처리하는 데이터 셋 추가

--- src/main/java/org/backrow/solt/domain/plan/api/ClovaApiResponse.java
+++ src/main/java/org/backrow/solt/domain/plan/api/ClovaApiResponse.java
@@ -1,4 +1,33 @@
 package org.backrow.solt.domain.plan.api;
 
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class ClovaApiResponse {
+    private Result result;
+    private List<PlacesResponses> places; // PlacesResponses 리스트
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @ToString
+    public static class Result {
+        private Message message;
+
+        @Data
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Builder
+        @ToString
+        public static class Message {
+            private String content;
+        }
+    }
 }
