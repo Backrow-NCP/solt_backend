a474f49903c508b44c3217c9596c9655c264b4ba
SD-PARK <aa55235490@gmail.com>
2024-10-21 15:44:51 +0900

    !HOTFIX: Place 및 Route의 시간 값 수정

--- src/main/java/org/backrow/solt/dto/plan/PlaceDTO.java
+++ src/main/java/org/backrow/solt/dto/plan/PlaceDTO.java
@@ -1,6 +1,7 @@
 package org.backrow.solt.dto.plan;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -22,10 +23,12 @@ public class PlaceDTO {
 
     private String category;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @Schema(type = "string", pattern = "yyyy-MM-dd'T'HH:mm:ss", example = "2024-10-21T05:42:37")
     private LocalDateTime startTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @Schema(type = "string", pattern = "yyyy-MM-dd'T'HH:mm:ss", example = "2024-10-21T05:42:37")
     private LocalDateTime endTime;
 
     @Builder.Default
--- src/main/java/org/backrow/solt/dto/plan/RouteDTO.java
+++ src/main/java/org/backrow/solt/dto/plan/RouteDTO.java
@@ -1,6 +1,7 @@
 package org.backrow.solt.dto.plan;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -12,10 +13,12 @@ import java.time.LocalDateTime;
 public class RouteDTO {
     private Long routeId;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @Schema(type = "string", pattern = "yyyy-MM-dd'T'HH:mm:ss", example = "2024-10-21T05:42:37")
     private LocalDateTime startTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @Schema(type = "string", pattern = "yyyy-MM-dd'T'HH:mm:ss", example = "2024-10-21T05:42:37")
     private LocalDateTime endTime;
 
     private Integer price;
