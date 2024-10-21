06046ee93795adff7a54ee44c82a388ad112ec5e
NoskeLim <NoskeLim>
2024-10-21 17:31:07 +0900

    Feat: Theme 사용을 위한 클래스 작성

---
+++ src/main/java/org/backrow/solt/repository/plan/ThemeRepository.java
@@ -0,0 +1,7 @@
+package org.backrow.solt.repository.plan;
+
+import org.backrow.solt.domain.plan.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+}
