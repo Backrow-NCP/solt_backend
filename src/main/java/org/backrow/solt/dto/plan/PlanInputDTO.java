81066c3b9078a2fdfa3bca2b966d39f5490fdea5
NoskeLim <NoskeLim>
2024-10-21 15:45:14 +0900

    Feat: @JsonInclude(JsonInclude.Include.NON_NULL) 추가

--- src/main/java/org/backrow/solt/dto/plan/PlanInputDTO.java
+++ src/main/java/org/backrow/solt/dto/plan/PlanInputDTO.java
@@ -1,5 +1,6 @@
 package org.backrow.solt.dto.plan;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 
@@ -11,6 +12,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PlanInputDTO {
     @NotEmpty(message = "일정명은 반드시 존재해야 합니다.")
     @Size(min = 1, max = 200, message = "일정명은 1~200자 이내의 문자열이어야 합니다.")
@@ -31,9 +33,7 @@ public class PlanInputDTO {
     @NotEmpty(message = "여행 장소는 반드시 존재해야 합니다.")
     private String location;
 
-    @NotNull(message = "시작 날짜는 반드시 존재해야 합니다.")
     private LocalDate startDate;
 
-    @NotNull(message = "종료 날짜는 반드시 존재해야 합니다.")
     private LocalDate endDate;
 }
