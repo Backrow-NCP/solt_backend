03ac66d0ad3b88b3674821230150f1d7c2922f39
NoskeLim <NoskeLim>
2024-10-21 21:20:51 +0900

    Feat: 이동수단 타입에 따른 가격 설정 로직 수정

--- src/main/java/org/backrow/solt/service/plan/TransportationUtil.java
+++ src/main/java/org/backrow/solt/service/plan/TransportationUtil.java
@@ -7,19 +7,24 @@ import java.util.List;
 public class TransportationUtil {
 
     // 이동 수단 ID를 대중교통을 한 번이라도 탔는지에 따라 가져오는 메소드
-    public static Integer getTransportationId(List<DirectionsResponses.Route.Step> steps) {
-        if (steps.isEmpty()) {
+    public static Integer getTransportationId(List<DirectionsResponses.Route.Leg> legs) {
+        if (legs.isEmpty()) {
             return 1; // 기본값: 도보 ID
         }
 
         // 대중교통을 탄 적이 있는지 확인
         boolean hasTransit = false;
 
-        for (DirectionsResponses.Route.Step step : steps) {
-            String mode = step.getTravelMode();
-            if ("TRANSIT".equalsIgnoreCase(mode)) {
-                hasTransit = true;
-                break; // 대중교통을 탔다면 더 이상 확인할 필요 없음
+        for (DirectionsResponses.Route.Leg leg : legs) {
+            for (DirectionsResponses.Route.Step step : leg.getSteps()) {
+                String mode = step.getTravelMode();
+                if ("TRANSIT".equalsIgnoreCase(mode)) {
+                    hasTransit = true;
+                    break; // 대중교통을 탔다면 더 이상 확인할 필요 없음
+                }
+            }
+            if (hasTransit) {
+                break;
             }
         }
 
@@ -28,34 +33,41 @@ public class TransportationUtil {
     }
 
     // 이동 수단 타입을 대중교통을 한 번이라도 탔는지에 따라 가져오는 메소드
-    public static String getTransportationType(List<DirectionsResponses.Route.Step> steps) {
-        if (steps.isEmpty()) {
+    public static String getTransportationType(List<DirectionsResponses.Route.Leg> legs) {
+        if (legs.isEmpty()) {
             return "도보"; // 기본값: 도보
         }
 
         // 대중교통을 탄 적이 있는지 확인
         boolean hasTransit = false;
 
-        for (DirectionsResponses.Route.Step step : steps) {
-            String mode = step.getTravelMode();
-            if ("TRANSIT".equalsIgnoreCase(mode)) {
-                hasTransit = true;
-                break; // 대중교통을 탔다면 더 이상 확인할 필요 없음
+        for (DirectionsResponses.Route.Leg leg : legs) {
+            for (DirectionsResponses.Route.Step step : leg.getSteps()) {
+                String mode = step.getTravelMode();
+                if ("TRANSIT".equalsIgnoreCase(mode)) {
+                    hasTransit = true;
+                    break; // 대중교통을 탔다면 더 이상 확인할 필요 없음
+                }
+            }
+            if (hasTransit) {
+                break;
             }
         }
 
         // 대중교통을 한 번이라도 탔다면 대중교통, 아니면 도보 반환
         if (hasTransit) {
             // 가장 처음 만나는 대중교통의 세부 정보를 반환
-            for (DirectionsResponses.Route.Step step : steps) {
-                if ("TRANSIT".equalsIgnoreCase(step.getTravelMode())) {
-                    DirectionsResponses.Route.Step.TransitDetails transitDetails = step.getTransitDetails();
-                    if (transitDetails != null) {
-                        String lineName = transitDetails.getLine().getName();
-                        String vehicleType = transitDetails.getLine().getVehicle().getName();
-                        return String.format("대중교통 (%s, %s)", lineName, vehicleType);
+            for (DirectionsResponses.Route.Leg leg : legs) {
+                for (DirectionsResponses.Route.Step step : leg.getSteps()) {
+                    if ("TRANSIT".equalsIgnoreCase(step.getTravelMode())) {
+                        DirectionsResponses.Route.Step.TransitDetails transitDetails = step.getTransitDetails();
+                        if (transitDetails != null) {
+                            String lineName = transitDetails.getLine().getName();
+                            String vehicleType = transitDetails.getLine().getVehicle().getName();
+                            return String.format("대중교통 (%s, %s)", lineName, vehicleType);
+                        }
+                        return "대중교통"; // 대중교통이지만 추가 정보가 없는 경우
                     }
-                    return "대중교통"; // 대중교통이지만 추가 정보가 없는 경우
                 }
             }
         }
