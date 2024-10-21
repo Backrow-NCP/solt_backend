69be9ff2103ef50132db180ecdde268a8de955a1
NoskeLim <NoskeLim>
2024-10-21 15:45:08 +0900

    Feat: @JsonInclude(JsonInclude.Include.NON_NULL) 추가

--- src/main/java/org/backrow/solt/dto/plan/PlaceDTO.java
+++ src/main/java/org/backrow/solt/dto/plan/PlaceDTO.java
@@ -1,17 +1,21 @@
 package org.backrow.solt.dto.plan;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PlaceDTO {
     private Long placeId;
 
+    @NotEmpty(message = "장소 이름은 반드시 입력되어야 합니다.")
     private String placeName;
 
     private String addr;
