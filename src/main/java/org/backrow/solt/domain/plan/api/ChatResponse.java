eb1dba6000f1e4495f1a1912a172f3b472d5ed16
NoskeLim <NoskeLim>
2024-10-22 21:14:06 +0900

    Feat: 응답을 받는 필드를 세분화

--- src/main/java/org/backrow/solt/domain/plan/api/ChatResponse.java
+++ src/main/java/org/backrow/solt/domain/plan/api/ChatResponse.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatResponse {
-
-    private String reply;
+    private String content; // assistant가 응답하는 내용
+    private String message; // 추가 메시지 또는 오류 메시지
 }
