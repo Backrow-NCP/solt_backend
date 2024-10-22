ae6a02f581d253736c49e80d024ba0866c568d20
NoskeLim <NoskeLim>
2024-10-22 19:20:51 +0900

    Feat: Chat 응답을 처리하는 클래스 생성

---
+++ src/main/java/org/backrow/solt/domain/plan/api/ChatResponse.java
@@ -0,0 +1,13 @@
+package org.backrow.solt.domain.plan.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatResponse {
+
+    private String reply;
+}
