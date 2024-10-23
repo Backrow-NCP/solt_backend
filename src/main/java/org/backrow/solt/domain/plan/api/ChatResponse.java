84518f4c80cd66a445d51133943996ccc027aa65
NoskeLim <NoskeLim>
2024-10-23 10:55:17 +0900

    Feat: ChatResponse 형식에 맞게 수정

--- src/main/java/org/backrow/solt/domain/plan/api/ChatResponse.java
+++ src/main/java/org/backrow/solt/domain/plan/api/ChatResponse.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ChatResponse {
     private String content; // assistant가 응답하는 내용
-    private String message; // 추가 메시지 또는 오류 메시지
+    private String error; // 추가 메시지 또는 오류 메시지
 
     private Status status; // API 응답 상태
     private Result result; // API 응답 결과
